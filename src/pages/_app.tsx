acde4cc8d7412d065639a929f06ef2621e4e31fe
Michael <m.eleyowo@clane.com>
2023-07-04 17:27:06 +0100

    small changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ConfigProvider, Alert } from "antd";
+import type { AppProps } from "next/app";
+import "antd/dist/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#7655DA",
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
